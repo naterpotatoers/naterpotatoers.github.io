5986e9ca1c07c8d0ec3393523733f79f9a27b620
naterpotatoer <nate.garza@hotmail.com>
2025-01-02 16:45:41 -0800

    :sparkles: adds partytown and google analytics logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nate-garza.dev',
-  integrations: [tailwind(), icon(), compress(), sitemap()]
+  integrations: [tailwind(), icon(), compress(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    }
+  })]
 });
